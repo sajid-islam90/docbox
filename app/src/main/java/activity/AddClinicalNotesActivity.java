502b760437fbdfc71e64b0df9628107547b10b7a
sajid_islam90 <sajid.islam90@gmail.com>
2015-10-20 18:55:32 +0530

    j

--- app/src/main/java/activity/AddClinicalNotesActivity.java
+++ app/src/main/java/activity/AddClinicalNotesActivity.java
@@ -25,66 +25,12 @@ public class AddClinicalNotesActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_clinical_notes);
-        TextView date = (TextView)findViewById(R.id.AddDate);
-        Calendar c = Calendar.getInstance();
 
-
-        SimpleDateFormat df = new SimpleDateFormat("dd-MMM-yyyy");
-        String formattedDate = df.format(c.getTime());
-        date.setText(formattedDate);
         Intent intent = getIntent();
         pid = intent.getIntExtra("id",0);
 
     }
 
-
-    public void saveNotes(View view)
-    {
-        TextView date = (TextView)findViewById(R.id.AddDate);
-        EditText complaints = (EditText)findViewById(R.id.AddChiefComplaint);
-        EditText presentIll = (EditText)findViewById(R.id.AddHistOfPresentIllness);
-        EditText pastHist = (EditText)findViewById(R.id.AddPastHistory);
-        EditText personalHist = (EditText)findViewById(R.id.AddPersonalHistory);
-        EditText familyHist = (EditText)findViewById(R.id.AddFamilyHistory);
-        EditText genExam = (EditText)findViewById(R.id.AddGeneralExamination);
-        EditText locExam = (EditText)findViewById(R.id.AddLocalExamination);
-        EditText classification = (EditText)findViewById(R.id.AddClassification);
-        EditText diagnosis = (EditText)findViewById(R.id.AddDiagnosis);
-        EditText treatment = (EditText)findViewById(R.id.AddTreatment);
-        EditText procedure = (EditText)findViewById(R.id.AddProcedure);
-        EditText implant = (EditText)findViewById(R.id.AddImplantUsed);
-        EditText score = (EditText)findViewById(R.id.AddScore);
-        EditText remark = (EditText)findViewById(R.id.AddRemark);
-
-        DatabaseHandler dbHandler = new DatabaseHandler(getApplicationContext());
-
-        notes_obj note = new notes_obj();
-        note.set_date(date.getText().toString());
-        note.set_complaint(complaints.getText().toString());
-        note.set_hist_present_illness(presentIll.getText().toString());
-        note.set_past_hist(pastHist.getText().toString());
-        note.set_personal_hist(personalHist.getText().toString());
-        note.set_family_hist(familyHist.getText().toString());
-        note.set_gen_exam(genExam.getText().toString());
-        note.set_loc_exam(locExam.getText().toString());
-        note.set_classification(classification.getText().toString());
-        note.set_diagnosis(diagnosis.getText().toString());
-        note.set_treatment(treatment.getText().toString());
-        //note.set_procedure(procedure.toString());
-        note.set_implant(implant.getText().toString());
-        note.set_score(score.getText().toString());
-        note.set_remark(remark.getText().toString());
-        //note.set_version(dbHandler.getCurrentVersion(pid)+1);
-        note.set_id(pid);
-
-        dbHandler.addNotes(note);
-        finish();
-
-
-
-
-
-    }
 
 
     @Override
@@ -106,12 +52,7 @@ public class AddClinicalNotesActivity extends ActionBarActivity {
         switch (item.getItemId()) {
             // Respond to the action bar's Up/Home button
             case android.R.id.home:
-               /* Intent intent1 = getIntent();
-                int Activity_id = intent1.getIntExtra("id",0);
-                Intent intent = new Intent(this,documents.class);
-                intent.putExtra("id",Activity_id);
-                startActivity(intent);
-                return true;*/
+
                 finish();
             case R.id.ViewHistory:
                 return true;
